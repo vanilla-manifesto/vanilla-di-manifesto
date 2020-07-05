082079577e0c8494ab1d78363084c252e851c66f
Yeongjun.Kim <yeongjun.kim@navercorp.com>
2020-07-05 15:39:11 +0900

    Cleanup kotlin code

--- kotlin/example/src/test/kotlin/vanilladi/BarSpy.kt
+++ kotlin/example/src/test/kotlin/vanilladi/BarSpy.kt
@@ -3,11 +3,8 @@ package vanilladi;
 class BarSpy : Bar() {
     private val calledArgs = mutableListOf<Int>();
 
-    fun getCalledArgs(): Iterable<Int> {
-        return calledArgs;
-    }
+    fun getCalledArgs(): Iterable<Int> = calledArgs
 
-    @Override
     override fun doSomething(value: Int) {
         calledArgs.add(value);
     }
